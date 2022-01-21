afd324aeb5eb69ff217d89ec2fe9cefcd1e077c4
raku10ehime <95738593+raku10ehime@users.noreply.github.com>
2022-01-22 00:40:00 +0900

    Update main.py

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ df["場所"] = df["場所"].mask(flag5G, "【5G】" + df["場所"])
 
 # 屋内
 df["icon"] = df["icon"].mask(df["設置タイプ"] == "屋内", "home")
-df["場所"] = df["場所"].mask(f["設置タイプ"] == "屋内", "【屋内】" + df["場所"])
+df["場所"] = df["場所"].mask(df["設置タイプ"] == "屋内", "【屋内】" + df["場所"])
 
 csv_path = pathlib.Path("map", "ehime.csv")
 df.to_csv(csv_path, encoding="utf_8_sig")
