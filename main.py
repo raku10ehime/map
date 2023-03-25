85a8a7919cc7ad1df16a2cd399675a39bf1f5cfd
raku10ehime <95738593+raku10ehime@users.noreply.github.com>
2023-03-25 19:27:58 +0900

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ dt_str = dt_now.strftime("%Y/%m/%d")
 
 df = (
     pd.read_csv(
-        url, usecols=[1, 2, 3, 5, 8, 9, 10, 11, 12, 13, 15, 16], dtype=str
+        url, usecols=[1, 2, 3, 5, 8, 9, 10, 11, 12, 13, 16, 17], dtype=str
     )
     .dropna(how="all")
     .fillna("")
